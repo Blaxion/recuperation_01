f400d3aac3e58241f61b34b44c724ee4c9fcd9b2
Blaxion <khibaziev.f@gmail.com>
2021-05-11 18:32:58 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,23 @@
+let h1_all = document.getElementsByTagName('h1')
+console.log(h1_all)
+
+
+let h3_all = document.getElementsByTagName('h3')
+console.log(h3_all)
+let titre_p1 = h3_all[0]
+let titre_p2 = h3_all[1]
+let titre_p3 = h3_all[2]
+console.log(titre_p1)
+console.log(titre_p2)
+console.log(titre_p3)
+
+let paragraphes = document.getElementsByTagName('p')
+console.log(paragraphes)
+let para1 = paragraphes[0]
+let para2 = paragraphes[1]
+console.log(para1)
+console.log(para2)
+
+let li_all = document.getElementsByTagName('li')
+let li_3 = li_all[2]
+console.log(li_3)
